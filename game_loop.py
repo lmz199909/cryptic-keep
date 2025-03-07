883e5d1855dc2703a211e730237ef027398026bc
lmz199909 <lmz2756861629@outlook.com>
2025-03-07 20:07:38 +0800

    Second

--- game_loop.py
+++
@@ -1,9 +0,0 @@
-from help import helps
-def Loop(s):
-    while True:
-        s=input("<").strip().lower()
-        if s==help:
-            helps()
-        elif s.startswith("go"):
-            s=s.split(" ")[1]
-            
